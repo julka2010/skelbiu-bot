b550d5ac5bc22f717c3ed5ce70db67654a2421f0
Julius Juodagalvis <julius.juodagalvis@protonmail.com>
2018-02-08 03:55:51 +0200

    All ads will be deleted before uploading new ones.

--- mybot/skelbiu/views.py
+++ mybot/skelbiu/views.py
@@ -16,6 +16,7 @@ def run_bot(request):
     driver = webdriver.Chrome()
     bot.login(driver)
     skelbiu = bot.Advertisement(driver)
+    skelbiu.delete_all()
     ads_to_publish_list = Advertisement.objects.all().filter(active=True)
     failed_ads = []
     with open('errors_log.csv', 'w') as errors_file:
